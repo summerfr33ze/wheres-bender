a6ed39da5e4e50500e1ca846e1cfa29973063cac
Trevor <trevorjamesmurphy@gmail.com>
2023-02-04 21:58:32 -0500

    trying yet again

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import {BrowserRouter as Router} from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router basename='/wheres-bender'>
+    <Router basename='/'>
     <App />
     </Router>
   </React.StrictMode>
