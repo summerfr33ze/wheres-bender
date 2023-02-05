915a4c6be3881e3b254ae9ffda95b3519b0f8f5f
Trevor <trevorjamesmurphy@gmail.com>
2023-02-04 21:52:36 -0500

    trying basename to fix github pages issue

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import {BrowserRouter as Router} from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <Router basename='/'>
     <App />
-    </HashRouter>
+    </Router>
   </React.StrictMode>
 );
